d19e8b74a3bb6dab3cbd2c10c9bd377f970a76ae
Sigfrido <sigfrido_o@ardilluda.com.mx>
2021-09-09 20:53:09 -0500

    CRUD.Productos: Implementando

--- tienda/urls.py
+++ tienda/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from productos.urls import productos_patterns
 
@@ -29,3 +30,9 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]
+
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
